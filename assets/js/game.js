b227a39e91fa896b7d25fc232b2018d004a9ad9d
jennj <jennjenn93@gmail.com>
2022-02-20 22:22:34 -0600

    Alternating attack

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,9 +28,19 @@ var fightOrSkip = function(){
         }
     }
 }
+
 var fight = function(enemy) {
+    // keep track of who goes first
+    var isPlayerTurn = true;
+
+    //randomly change turn order
+    if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+    }
+
     //repeat and sxecute as long as the enemy-robot is alive
-    while(playerInfo.health > 0 && enemy.health > 0) {
+    while (playerInfo.health > 0 && enemy.health > 0) {
+        if (isPlayerTurn) {
     // ask player if they'd like to fight or skip using fightOrSkip function
         if (fightOrSkip()) {
             //if true, leave fight by breaking loop
@@ -51,12 +61,17 @@ var fight = function(enemy) {
     //check enemy's health
     if (enemy.health <= 0) {
         window.alert(enemy.name + " has died!");
+
+        //award player for winning
+        playerInfo.money = playerInfo.money + 20;
+
+        //leave while() loop since enemy is dead
         break;
     }
     else {
         window.alert(enemy.name + " still has " + enemy.health + " health left.");
     }
-
+    //player gets attacked first
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
 
@@ -77,6 +92,9 @@ var fight = function(enemy) {
         window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
     }
     }
+    //switch turn order for next round
+    isPlayerTurn = !isPlayerTurn;
+}
 };
 
 //function to start a new game
